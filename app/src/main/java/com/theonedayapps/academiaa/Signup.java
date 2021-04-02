f50215c325440fd7213926520d94ff16701d4b8f
atharva_dawande <atharva.dawande603@gmail.com>
2021-04-03 01:00:18 +0530

    firebase auth create user working in button

--- app/src/main/java/com/theonedayapps/academiaa/Signup.java
+++ app/src/main/java/com/theonedayapps/academiaa/Signup.java
@@ -19,6 +19,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class Signup extends AppCompatActivity {
 private Button back;
+private Button signup;
     private FirebaseAuth mAuth;
     private static final String TAG = "EmailPassword";
 
@@ -30,11 +31,53 @@ private Button back;
 
         mAuth = FirebaseAuth.getInstance();
         back =findViewById(R.id.backbutton);
+        signup =findViewById(R.id.signup_button);
 ////////////////////////////////////////////////////////////////////////
 
 String email="qqq@atha.com";
 String password="jhjhjhjh";
 
+
+
+
+
+        ////////////////////////////////////////////////////////////////
+        back.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent int1 =new Intent(Signup.this,MainActivity.class);
+
+        // set the new task and clear flags
+        int1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);      //Android: Remove all the previous activities from the back stack
+
+
+        startActivity(int1);
+    }
+});
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAccount("abc1@atha.com", "password");
+                Intent intent=new Intent(Signup.this,loginact.class);
+
+                startActivity(intent);
+            }
+        });
+
+       // createAccount("abc1@atha.com", "password");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void createAccount(String email, String password) {
+        // [START create_user_with_email]
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -53,37 +96,6 @@ String password="jhjhjhjh";
                         }
                     }
                 });
-
-
-
-        ////////////////////////////////////////////////////////////////
-        back.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        Intent int1 =new Intent(Signup.this,MainActivity.class);
-
-        // set the new task and clear flags
-        int1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);      //Android: Remove all the previous activities from the back stack
-
-
-        startActivity(int1);
-    }
-});
-
-       // createAccount("abc@atha.com", "password");
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
-    }
-
-    private void createAccount(String email, String password) {
-        // [START create_user_with_email]
-
         // [END create_user_with_email]
     }
     private void reload() { }
