271dba80e4d13048507ecb19c2e2686656ddfe10
aishwaryamankar <aishwaryamankar2000@gmail.com>
2021-04-09 23:45:41 +0530

    Home Activity Data

--- app/src/main/java/com/theonedayapps/academiaa/Useractivity.java
+++ app/src/main/java/com/theonedayapps/academiaa/Useractivity.java
@@ -2,13 +2,24 @@ package com.theonedayapps.academiaa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Useractivity extends AppCompatActivity {
-
+    private Button move;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_useractivity);
+        move=findViewById(R.id.ais_move);
+        move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Useractivity.this,User_Info_Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
