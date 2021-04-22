a18b898eebed81d7f07150af8c8d8d1535f99f13
atharva_dawande <atharva.dawande603@gmail.com>
2021-04-23 02:38:36 +0530

    cahnges in ui and created a class to use user uid again and again

--- app/src/main/java/com/theonedayapps/academiaa/Useractivity.java
+++ app/src/main/java/com/theonedayapps/academiaa/Useractivity.java
@@ -7,16 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Useractivity extends AppCompatActivity {
     private Button move;
+    private TextView textuidtemp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_useractivity);
         move=findViewById(R.id.ais_move);
+        textuidtemp=findViewById(R.id.textView7);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
+        Firebase_verification obj1=new Firebase_verification();
+        textuidtemp.setText(obj1.getFirebase_uid());
         move.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
