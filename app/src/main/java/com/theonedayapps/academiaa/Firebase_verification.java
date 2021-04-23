02c955c6a655f70790947fa3e5d23e1c31975680
atharva_dawande <atharva.dawande603@gmail.com>
2021-04-23 13:29:27 +0530

    main page ui updated

---
+++ app/src/main/java/com/theonedayapps/academiaa/Firebase_verification.java
@@ -0,0 +1,13 @@
+package com.theonedayapps.academiaa;
+
+public class Firebase_verification {
+private static String firebase_uid;
+
+    public static String getFirebase_uid() {
+        return firebase_uid;
+    }
+
+    public static void setFirebase_uid(String firebase_uid) {
+        Firebase_verification.firebase_uid = firebase_uid;
+    }
+}
