65ae805c3aa4c65c449cdb1cd29b0e1a86ee510b
atharva_dawande <atharva.dawande603@gmail.com>
2021-05-14 13:00:34 +0530

    timetable data

--- app/src/main/java/com/theonedayapps/academiaa/ui2/time_table/Time_table.java
+++ app/src/main/java/com/theonedayapps/academiaa/ui2/time_table/Time_table.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,10 +16,17 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.theonedayapps.academiaa.R;
+import com.theonedayapps.academiaa.Useractivity;
 import com.theonedayapps.academiaa.ui2.notice.NoticeViewModel;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import lecho.lib.hellocharts.model.PieChartData;
@@ -28,7 +36,7 @@ import lecho.lib.hellocharts.view.PieChartView;
 public class Time_table extends Fragment {
 
     private Time_tableViewModel time_tableViewModel;
-private TextView timetable_date,timetable_lecture;
+private TextView timetable_date,timetable_lecture,rollnumber;
 private PieChartView pieChartView;
 private CalendarView calendarView;
 private String selected_date;
@@ -47,7 +55,7 @@ private String selected_date;
                 timetable_lecture=root.findViewById(R.id.lectures_timetable);
                 pieChartView=root.findViewById(R.id.chart_timetable);
                 calendarView=root.findViewById(R.id.calendarView);
-
+                rollnumber=root.findViewById(R.id.textViewroll);
                 calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
 
                     @Override
@@ -56,7 +64,7 @@ private String selected_date;
                         selected_date = String.valueOf(dayOfMonth)+String.valueOf(month+1)+String.valueOf(year);
                         Log.d("##############date", selected_date);
                         timetable_date.setText(selected_date);
-
+                        firebasefn((String) rollnumber.getText(),"1","A",selected_date,timetable_lecture,pieChartView);
                     }
                 });
 
@@ -74,11 +82,49 @@ private String selected_date;
         pieData.add(new SliceValue(data, Color.parseColor("#2D98FB")).setLabel(""));
 
 
-        pieData.add(new SliceValue(total, Color.parseColor("#796E57E8")).setLabel(""));
+        pieData.add(new SliceValue(6, Color.parseColor("#796E57E8")).setLabel(""));
 
         PieChartData pieChartData = new PieChartData(pieData);
         pieChartData.setHasLabels(true).setValueLabelTextSize(14);
         pieChartData.setHasCenterCircle(true).setCenterText1(settext1).setCenterText1FontSize(20).setCenterText1Color(Color.parseColor("#0097A7"));
         pieChartView.setPieChartData(pieChartData);
+    }
+
+
+    void firebasefn(String roll,String year,String division,String date,TextView a,PieChartView pie){
+        DatabaseReference myRef;
+        // Firebase_verification obj=new Firebase_verification();
+        myRef = FirebaseDatabase.getInstance().getReference();
+        Useractivity obj3=new Useractivity();
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String str = dataSnapshot.child("Classroom").child(roll).child(year).child(division).child(date).getValue().toString();
+                //roll_no=value;
+                List<String> items = Arrays.asList(str.split("\\s*,\\s*"));
+                String app="";
+                for (int i = 0; i < items.size(); i++) {
+
+                    app=app+System.lineSeparator()+items.get(i);
+                }
+                piechartfun(items.size(), 2,String.valueOf(items.size())+"/8",pie);
+                a.setText(app);
+                Log.d("#####value check", "Value is: " + app);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("value check 2", "Failed to read value.", error.toException());
+
+            }
+        });
+
+
+
     }
 }
