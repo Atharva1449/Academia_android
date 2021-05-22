9dd1676e08bd0431f9aa3c83190139c64e82ce84
atharva_dawande <atharva.dawande603@gmail.com>
2021-05-22 12:38:27 +0530

    working recycler view

--- app/src/main/java/com/theonedayapps/academiaa/ui2/notice/Notice.java
+++ app/src/main/java/com/theonedayapps/academiaa/ui2/notice/Notice.java
@@ -10,13 +10,20 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.theonedayapps.academiaa.R;
 
 public class Notice extends Fragment {
 
     private NoticeViewModel noticeViewModel;
-
+    private RecyclerView recyclerView;
+    NoticehandleAdapter adapter;
+    DatabaseReference mbase;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         noticeViewModel =
@@ -27,8 +34,44 @@ public class Notice extends Fragment {
             @Override
             public void onChanged(@Nullable String s) {
 
+              //  mbase= FirebaseDatabase.getInstance().getReference().child("Notice");
+
+
             }
+
         });
+        recyclerView = root.findViewById(R.id.recycler1);
+
+        // To display the Recycler view linearly
+        recyclerView.setLayoutManager(
+                new LinearLayoutManager(getActivity()));
+
+        // It is a class provide by the FirebaseUI to make a
+        // query in the database to fetch appropriate data
+        FirebaseRecyclerOptions<Noticehandle> options
+                = new FirebaseRecyclerOptions.Builder<Noticehandle>()
+                .setQuery(FirebaseDatabase.getInstance().getReference().child("Notice"), Noticehandle.class)
+                .build();
+        // Connecting object of required Adapter class to
+        // the Adapter class itself
+        adapter = new NoticehandleAdapter(options);
+        // Connecting Adapter class with the Recycler view*/
+        recyclerView.setAdapter(adapter);
+
+
         return root;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+    ///s
 }
---
+++ app/src/main/java/com/theonedayapps/academiaa/ui2/notice/Noticehandle.java
@@ -0,0 +1,54 @@
+package com.theonedayapps.academiaa.ui2.notice;
+
+public class Noticehandle {
+    private String content1;
+    private String date;
+    private String link;
+    private String title1;
+    public String getContent1() {
+        return content1;
+    }
+
+    public void setContent1(String content1) {
+        this.content1 = content1;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getTitle1() {
+        return title1;
+    }
+
+    public void setTitle1(String title1) {
+        this.title1 = title1;
+    }
+
+    public Noticehandle() {
+    }
+
+    public Noticehandle(String content1, String date, String link, String title1) {
+        this.content1 = content1;
+        this.date = date;
+        this.link = link;
+        this.title1 = title1;
+    }
+
+
+
+
+
+}
